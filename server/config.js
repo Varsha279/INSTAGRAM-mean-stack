3194e3e8399ea4676d7d1ebddd0c959e66dd898c
Varsha Ubhrani <ubhrani.varsha@gmail.com>
2017-06-01 15:58:26 -0700

    First Commit

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	db:'localhost',
+	clientSecret: process.env.clientSecret || 'c330b47a76df49a5b963f5a70f48af9f ',
+	tokenSecret:process.env.tokenSecret || '455787j8io90pl7jud67'
+}
