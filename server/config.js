ab26557576e89694cd81ee3111d0abb7bf93cc8d
Varsha Ubhrani <ubhrani.varsha@gmail.com>
2017-06-03 12:11:28 -0700

    node

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	db:'localhost',
+	db:process.env.db || 'localhost',
 	clientSecret: process.env.clientSecret || 'c330b47a76df49a5b963f5a70f48af9f ',
 	tokenSecret:process.env.tokenSecret || '455787j8io90pl7jud67'
 }
